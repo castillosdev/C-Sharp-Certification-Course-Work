2d03f277731627784ebe6067e9b86d0eee3e86f6
Louis Castillo <louiscastillo.dev@outlook.com>
2023-09-05 11:14:58 -0400

    added completed course work mod 1-2

---
+++ Module 2 - Create and Run Simple C# Console Applications/FruadulentOrderChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿//This challenge is to filter out fraudulent orders from a list of orders. The fraudulent orders all start with the letter "B". The list of fraudulent orders is stored in the fraudulentOrderIDs array.
+//The code should print the OrderID for any fraudulent order.
+
+string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (var item in fraudulentOrderIDs)
+{
+   if (item.StartsWith("B"))
+   {
+       Console.WriteLine(item);
+   }
+}
---
+++ Module 2 - Create and Run Simple C# Console Applications/ImproveRenewalRateOfSubs/Program.cs
@@ -0,0 +1,38 @@
+﻿//This code is supposed  to give a discount to users who renew their subscription early. The discount is determined by the number of days remaining on their subscription. The number of days remaining is stored in the daysUntilExpiration variable.
+// The code should print the following messages:
+// Your subscription expires within a day!
+// Renew now and save 20%!
+// Your subscription expires in 5 days.
+// Renew now and save 10%!
+// Your subscription expires in 10 days.
+// Your subscription will expire soon.  Renew now!
+// Your subscription has expired.
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+}
+else
+{
+    Console.WriteLine("Your subscription will expire soon.  Renew now!");
+}
